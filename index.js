1e3e37538fed7c1780a4d06c439ae567bb6e6b0d
Bvyshnavi2026 <242FA04203@vignan.ac.in>
2026-09-17 10:29:44 +0530

    third commit

--- index.js
+++ index.js
@@ -2,11 +2,20 @@ let express=require("express");
 let app=express();
 let hrroutes=require('./routes/hr_route');
 let emproutes=require('./routes/emp_route');
+let mongoose=require('mongoose');
+mongoose.connect("mongodb://127.0.0.1:27017/hrmanagement")
+.then(()=>{
+    console.log("connected with mongodb database")
+}).catch((err)=>{
+    console.log(err);
+})
+app.use(express.json());
+// used for collecting input from json data 
 app.use("/api/hr",hrroutes);
-app.use("/api/employee",emproutes);
+app.use("/api/emp",emproutes);
 // localhost:3000/api/hr/viewtasks
 // localhost:3000/api/hr/viewemployees
-// run the server
+// run the server     
 app.listen(3000,()=>{
     console.log("server listening on port 3000")
 })
--- routes/emp_route.js
+++ routes/emp_route.js
@@ -1,13 +1,18 @@
 let express=require('express');
 let router=express.Router();
+let {users}=require('../models/users');
 router.get("/viewtask",(req,res)=>{
     res.send("view task route");
 });
 router.post("/login",(req,res)=>{
     res.send("login  route");
 });
-router.post("/register",(req,res)=>{
-    res.send("register route");
+router.post("/register",async(req,res)=>{
+    //  whatever we enter in postman input data collecting
+    console.log(req.body);
+    let newuser=users(req.body);
+    let result= await newuser.save();
+    res.send(result);
 });
 router.put("/updatestatus",(req,res)=>{
     res.send("update status  route");
